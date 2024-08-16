eafa2621a0b26242ead38dd915721fc8f9655fe1
KhaledAMARI <kha3d.amari@gmail.com>
2024-08-17 00:43:44 +0100

    - Add poppins font. - WIP Navbar: Add second part of navigation bar.

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,22 +1,27 @@
 import React from "react";
-import profileIcon from "../assets/profile_close_2.svg";
-import cartIcon from "../assets/cart_icon.svg";
-import searchIcon from "../assets/search_icon.svg";
+import profileIcon from "../assets/images/profile_close_2.svg";
+import cartIcon from "../assets/images/cart_icon.svg";
+import searchIcon from "../assets/images/search_icon.svg";
+import logo from "../assets/images/logo.svg";
 
 const Layout = () => {
   return (
-    <main className="min-w-full min-h-full grid grid-rows-3 grid-flow-col">
-      <nav className="grid grid-cols-2 grid-flow-col h-[42px] place-content-center">
+    <main className="min-w-full min-h-full grid grid-rows-3 grid-flow-row bg-white">
+      <nav className="grid grid-cols-2 grid-flow-col h-[42px] place-content-center  p-11">
         <div className="grid grid-cols-2 w-28">
           <select className=" w-[38.26px] h-6 bg-transparent">
             <option value="ar">AR</option>
-            <option value="en">EN</option>
+            <option value="en" selected>
+              EN
+            </option>
             <option value="fr">FR</option>
           </select>
-          <select className=" w-[38.26px] h-6 bg-transparent">
-            <option value="ar">TND</option>
-            <option value="en">USD</option>
-            <option value="fr">EURO</option>
+          <select className=" w-[50.85px] h-6 bg-transparent">
+            <option value="tnd">TND</option>
+            <option value="usd" selected>
+              USD
+            </option>
+            <option value="eur">EUR</option>
           </select>
         </div>
         <div className="grid grid-cols-4 gap-1">
@@ -51,6 +56,44 @@ const Layout = () => {
               className="w-[20.95px] h-[20.94px]"
             />
           </div>
+        </div>
+      </nav>
+      <nav className="grid grid-cols-2 grid-flow-col place-content-center h-11  p-11">
+        <div className="grid grid-flow-col place-items-center w-[134.32px] h-11">
+          <img src={logo} alt="logo" className="w-11 h-11" />
+          <span className="w-[84px] h-[27px] font-bold text-lg]">E-Comm</span>
+        </div>
+        <div className="w-[815.49px] h-9 grid grid-flow-col gap-2">
+          <button
+            type="button"
+            className="text-[#33A0FF] w-[70px] h-9 font-medium text-2xl font-poppins"
+          >
+            HOME
+          </button>
+          <button
+            type="button"
+            className="w-[70px] h-9 font-medium text-2xl font-poppins"
+          >
+            BAGS
+          </button>
+          <button
+            type="button"
+            className="w-[70px] h-9 font-medium text-2xl font-poppins"
+          >
+            SNEAKERS
+          </button>
+          <button
+            type="button"
+            className="w-[70px] h-9 font-medium text-2xl font-poppins"
+          >
+            BELT
+          </button>
+          <button
+            type="button"
+            className="w-[70px] h-9 font-medium text-2xl font-poppins"
+          >
+            CONTACT
+          </button>
         </div>
       </nav>
       <section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
@@ -6,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'poppins': ['Poppins-Regular']
+    }
   },
   plugins: [],
 }
